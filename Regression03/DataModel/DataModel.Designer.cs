5824a1546b1e07d55dc33432aefa649c8ea9036b
deverett <deverett@apexedi.com>
2013-03-19 08:14:50 -0600

    Modified the data model to only include necessary tables

---
+++ Regression03/DataModel/DataModel.Designer.cs
@@ -0,0 +1,2 @@
+﻿// Seeing this comment implies that the code generation for file 'C:\Users\deverett.APEX\repository\Regression03\DataModel\DataModel.edmx'
+// failed. See the ErrorList for details.
